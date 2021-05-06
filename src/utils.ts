5abeab43710b95dbba14d14a7b406cf3d6017c45
Sean O'Reilly <soreilly@spoton.com>
2021-05-05 23:54:54 -0700

    cleanup comments with confidence that this is the compare algorithm yolo

--- src/utils.ts
+++ src/utils.ts
@@ -77,6 +77,120 @@ export const COLORS = [
 ];
 // shuffleArray(COLORS);
 
+function _rotatedDistance(a: number, b: number, m: number = 12) {
+	let min = a - b;
+	if (Math.abs(a - b + m) < Math.abs(min)) min = a - b + m;
+	if (Math.abs(a - b - m) < Math.abs(min)) min = a - b - m;
+	return min;
+}
+
+function _getMinDiff(
+	longerList: number[],
+	shorterList: number[]
+): [number, DiffType] {
+	let minSum;
+	let minDiff = {};
+	let minDiffScore;
+	if (longerList.length === shorterList.length) {
+		// return min sum and Diff for best rotation
+		const rotatedIndexes = longerList.map((_, i) => (j: number) =>
+			(j + i) % longerList.length
+		);
+
+		for (let rotation of rotatedIndexes) {
+			let diff: DiffType = {};
+			let diffScore;
+			let absSum;
+
+			for (let i = 0; i < longerList.length; i++) {
+				const j = rotation(i);
+				const distance = _rotatedDistance(
+					shorterList[j],
+					longerList[i]
+				);
+				if (diffScore === undefined) diffScore = 0;
+				if (absSum === undefined) absSum = 0;
+				diffScore += distance * distance;
+				absSum += Math.abs(distance);
+				diff[longerList[i]] = distance;
+			}
+
+			const overallMovementIsLess =
+				minSum === undefined || absSum < minSum;
+			const lateralMovementIsLess =
+				minSum !== undefined &&
+				absSum === minSum &&
+				diffScore < minDiffScore;
+
+			if (overallMovementIsLess || lateralMovementIsLess) {
+				minSum = absSum;
+				minDiff = diff;
+				minDiffScore = diffScore;
+			}
+		}
+	} else {
+		// remove each element of longerList and call compare on this reduced version (dynamic programming solution)
+		// return min result from each of these
+		for (let i = 0; i < longerList.length; i++) {
+			const reducedList = longerList
+				.slice(0, i)
+				.concat(longerList.slice(i + 1));
+			const [sum, diff] = _getMinDiff(reducedList, shorterList);
+			if (minSum === undefined || sum < minSum) {
+				minSum = sum;
+				minDiff = diff;
+			}
+		}
+	}
+
+	return [minSum, minDiff];
+}
+
+export function compare(fretboardA: FretboardUtil, fretboardB: FretboardUtil) {
+	// for generating diffs between fretboardA and fretboardB in both directions
+	let listA = fretboardA.list();
+	let listB = fretboardB.list();
+	const listBLonger = listA.length < listB.length;
+	let longer = listBLonger ? listB : listA;
+	let shorter = listBLonger ? listA : listB;
+
+	const [_, diff] = _getMinDiff(longer, shorter);
+
+	// build both directions
+	const longToShort = diff;
+	const shortToLong: DiffType = {};
+	const toIndexes: Set<number> = new Set();
+
+	Object.keys(longToShort).forEach((fromIndex) => {
+		const distance = longToShort[+fromIndex];
+		const toIndex = mod(+fromIndex + distance, 12);
+		toIndexes.add(toIndex);
+		shortToLong[toIndex] = -distance;
+	});
+
+	// 9999 = fill
+	// -9999 = empty
+	for (let index of longer) {
+		if (longToShort[+index] === undefined) {
+			longToShort[+index] = -9999;
+			shortToLong[+index] = 9999;
+		}
+	}
+
+	for (let index of shorter) {
+		if (!toIndexes.has(+index)) {
+			longToShort[+index] = 9999;
+			shortToLong[+index] = -9999;
+		}
+	}
+
+	// leftDiff is for the "fretboard on the right" which needs a diff against its left neighor
+	// rightDiff is for the "fretboard on the left" which needs a diff against its right neighor
+	let leftDiff = listBLonger ? longToShort : shortToLong;
+	let rightDiff = listBLonger ? shortToLong : longToShort;
+	return [leftDiff, rightDiff];
+}
+
 export class NoteUtil {
 	base: number;
 	constructor(base: number) {
@@ -168,93 +282,6 @@ export class FretboardUtil implements FretboardUtilType {
 			.sort((a: number, b: number) => a - b);
 	}
 
-	// compare2(fretboard: FretboardUtil) {
-	// 	// simple implementation for now, needs to be refined
-	// 	const list = this.list();
-	// 	const compareList = fretboard.list();
-	// 	const result: { [key: number]: number } = {};
-
-	// 	// direct compare
-	// 	for (let i = 0; i < list.length; i++) {
-	// 		let min;
-	// 		for (let j = 0; j < compareList.length; j++) {
-	// 			const distance = _rotatedDistance(compareList[j], list[i], 12);
-	// 			if (min === undefined || distance * distance <= min * min) {
-	// 				min = distance;
-	// 			}
-	// 		}
-	// 		result[list[i]] = min;
-	// 	}
-
-	// 	return result;
-	// }
-
-	// compare3(fretboard: FretboardUtil) {
-	// 	const list = this.list();
-	// 	const compareList = fretboard.list();
-
-	// 	// available notes left
-	// 	const available: { [key in number]: boolean } = {};
-	// 	compareList.forEach((index) => (available[index] = true));
-
-	// 	let minItems = [];
-	// 	for (let i = 0; i < list.length; i++) {
-	// 		let distances = compareList.map((index) => ({
-	// 			toIndex: index,
-	// 			distance: this._rotatedDistance(index, list[i]),
-	// 		}));
-	// 		if (distances.length) {
-	// 			distances.sort(
-	// 				(a, b) => Math.abs(a.distance) - Math.abs(b.distance)
-	// 			);
-
-	// 			distances = distances.filter(
-	// 				(distance) =>
-	// 					Math.abs(distance.distance) ===
-	// 					Math.abs(distances[0].distance)
-	// 			);
-
-	// 			minItems.push({
-	// 				distance: distances.map((distance) => distance.distance),
-	// 				fromIndex: list[i],
-	// 				toIndex: distances.map((distance) => distance.toIndex),
-	// 			});
-	// 		}
-	// 	}
-
-	// 	minItems.sort(
-	// 		(a, b) => Math.abs(a.distance[0]) - Math.abs(b.distance[0])
-	// 	);
-
-	// 	const result: DiffType = {};
-
-	// 	for (let item of minItems) {
-	// 		const { fromIndex, toIndex, distance } = item;
-	// 		for (let i = 0; i < toIndex.length; i++) {
-	// 			const index = toIndex[i];
-	// 			const d = distance[i];
-	// 			if (available[index]) {
-	// 				result[fromIndex] = d;
-	// 				available[index] = false;
-	// 				break;
-	// 			}
-	// 		}
-	// 	}
-
-	// 	for (let index of list) {
-	// 		if (result[index] === undefined) result[index] = -9999;
-	// 	}
-
-	// 	// maybe fix for later
-	// 	for (let index of Object.keys(available)) {
-	// 		if (available[+index]) {
-	// 			result[+index] = 9999;
-	// 		}
-	// 	}
-
-	// 	return result;
-	// }
-
 	_getIncrement(value: number, inc: number, scale: number[]): number {
 		if (inc === 0) return value;
 
@@ -306,50 +333,46 @@ export class FretboardUtil implements FretboardUtilType {
 			: 0;
 
 		let valid = true;
-		for (let stringIndex in this.strings) {
+
+		loop1: for (let stringIndex in this.strings) {
 			const newStringIndex: number = vertical
 				? +stringIndex + inc
 				: +stringIndex;
 			for (let fretValue in this.strings[stringIndex]) {
-				if (this.getFret(stringIndex, fretValue)) {
-					let newValue: number;
-					let conditions: boolean[];
-					if (vertical) {
-						// get most vertical interval step, find new note value, and apply to next string
-						newValue = this._getIncrement(
-							+fretValue,
-							verticalIntervalIncrement,
-							scale
-						);
-						conditions = [
-							newStringIndex < 0,
-							newStringIndex >= this.strings.length,
-							newValue < STANDARD_TUNING[newStringIndex],
-							newValue >=
-								STANDARD_TUNING[newStringIndex] + STRING_SIZE,
-						];
-					} else {
-						// find new note value, and apply to same string
-						newValue = this._getIncrement(+fretValue, inc, scale);
-						conditions = [
-							newValue === +fretValue,
-							newValue < STANDARD_TUNING[stringIndex],
-							newValue >=
-								STANDARD_TUNING[stringIndex] + STRING_SIZE,
-						];
-					}
-
-					if (conditions.some((_) => _)) {
-						valid = false;
-						break;
-					}
-
-					turnOff.push([stringIndex, fretValue]);
-					turnOn.push([newStringIndex, newValue]);
+				if (!this.getFret(stringIndex, fretValue)) continue;
+				let newValue: number;
+				let conditions: boolean[];
+				if (vertical) {
+					// get most vertical interval step, find new note value, and apply to next string
+					newValue = this._getIncrement(
+						+fretValue,
+						verticalIntervalIncrement,
+						scale
+					);
+					conditions = [
+						newStringIndex < 0,
+						newStringIndex >= this.strings.length,
+						newValue < STANDARD_TUNING[newStringIndex],
+						newValue >=
+							STANDARD_TUNING[newStringIndex] + STRING_SIZE,
+					];
+				} else {
+					// find new note value, and apply to same string
+					newValue = this._getIncrement(+fretValue, inc, scale);
+					conditions = [
+						newValue === +fretValue,
+						newValue < STANDARD_TUNING[stringIndex],
+						newValue >= STANDARD_TUNING[stringIndex] + STRING_SIZE,
+					];
 				}
-			}
-			if (!valid) {
-				break;
+
+				if (conditions.some((_) => _)) {
+					valid = false;
+					break loop1;
+				}
+
+				turnOff.push([stringIndex, fretValue]);
+				turnOn.push([newStringIndex, newValue]);
 			}
 		}
 
@@ -377,359 +400,3 @@ export class FretboardUtil implements FretboardUtilType {
 		};
 	}
 }
-
-// const getFretboardDiff = (left = false) => {
-// 	const currentFretboard = state.fretboards[fretboardIndex];
-// 	if (left && fretboardIndex === 0) return;
-// 	if (!left && fretboardIndex === state.fretboards.length - 1) return;
-
-// 	const targetFretboard =
-// 		state.fretboards[fretboardIndex + (left ? -1 : 1)];
-
-// 	return currentFretboard.compare(targetFretboard);
-// };
-
-// leftDiffRef.current = getFretboardDiff(true);
-// rightDiffRef.current = getFretboardDiff(false);
-
-export function getMinDiff(
-	longerList: number[],
-	shorterList: number[]
-): [number, DiffType] {
-	let minSum;
-	let minDiff = {};
-	let minDiffScore;
-	if (longerList.length === shorterList.length) {
-		// return min sum and Diff for best rotation
-		const rotatedIndexes = longerList.map((_, i) => (j: number) =>
-			(j + i) % longerList.length
-		);
-
-		for (let rotation of rotatedIndexes) {
-			let diff: DiffType = {};
-			let diffScore;
-			let absSum;
-
-			for (let i = 0; i < longerList.length; i++) {
-				const j = rotation(i);
-				const distance = _rotatedDistance(
-					shorterList[j],
-					longerList[i]
-				);
-				if (diffScore === undefined) diffScore = 0;
-				if (absSum === undefined) absSum = 0;
-				diffScore += distance * distance;
-				absSum += Math.abs(distance);
-				diff[longerList[i]] = distance;
-			}
-
-			const overallMovementIsLess =
-				minSum === undefined || absSum < minSum;
-			const lateralMovementIsLess =
-				minSum !== undefined &&
-				absSum === minSum &&
-				diffScore < minDiffScore;
-
-			if (overallMovementIsLess || lateralMovementIsLess) {
-				minSum = absSum;
-				minDiff = diff;
-				minDiffScore = diffScore;
-			}
-		}
-	} else {
-		// remove each element of longerList and call compare on this reduced version (dynamic programming solution)
-		// return min result from each of these
-		for (let i = 0; i < longerList.length; i++) {
-			const reducedList = longerList
-				.slice(0, i)
-				.concat(longerList.slice(i + 1));
-			const [sum, diff] = getMinDiff(reducedList, shorterList);
-			if (minSum === undefined || sum < minSum) {
-				minSum = sum;
-				minDiff = diff;
-			}
-		}
-	}
-
-	return [minSum, minDiff];
-}
-
-export function compare(fretboardA: FretboardUtil, fretboardB: FretboardUtil) {
-	let listA = fretboardA.list();
-	let listB = fretboardB.list();
-	const listBLonger = listA.length < listB.length;
-	let longer = listBLonger ? listB : listA;
-	let shorter = listBLonger ? listA : listB;
-
-	const [_, diff] = getMinDiff(longer, shorter);
-
-	// build both directions
-	const longToShort = diff;
-	const shortToLong: DiffType = {};
-	const toIndexes: Set<number> = new Set();
-
-	Object.keys(longToShort).forEach((fromIndex) => {
-		const distance = longToShort[+fromIndex];
-		const toIndex = mod(+fromIndex + distance, 12);
-		toIndexes.add(toIndex);
-		shortToLong[toIndex] = -distance;
-	});
-
-	// 9999 = fill
-	// -9999 = empty
-	for (let index of longer) {
-		if (longToShort[+index] === undefined) {
-			longToShort[+index] = -9999;
-			shortToLong[+index] = 9999;
-		}
-	}
-
-	for (let index of shorter) {
-		if (!toIndexes.has(+index)) {
-			longToShort[+index] = 9999;
-			shortToLong[+index] = -9999;
-		}
-	}
-
-	// leftDiff is for the "fretboard on the right" which needs a diff against its left neighor
-	// rightDiff is for the "fretboard on the left" which needs a diff against its right neighor
-	let leftDiff = listBLonger ? longToShort : shortToLong;
-	let rightDiff = listBLonger ? shortToLong : longToShort;
-	return [leftDiff, rightDiff];
-}
-
-// export function compare(fretboardA: FretboardUtil, fretboardB: FretboardUtil) {
-// 	// returns dict of fretboard notes that need to change between two fretboards,
-// 	// how far they move or if they are vanishing/appearing
-
-// 	const list = fretboardA.list();
-// 	const compareList = fretboardB.list();
-
-// 	// available notes left
-// 	const available: { [key in number]: boolean } = {};
-// 	compareList.forEach((index) => (available[index] = true));
-
-// 	// if (
-// 	// 	// fretboard.notes[0] &&
-// 	// 	fretboard.notes[2] &&
-// 	// 	fretboard.notes[4] &&
-// 	// 	fretboard.notes[6]
-// 	// 	// fretboard.notes[9]
-// 	// ) {
-// 	// 	console.log("in here");
-// 	// 	debugger;
-// 	// }
-
-// 	let minItems: Array<{
-// 		fromIndex: number;
-// 		distances: Array<{ toIndex: number; distance: number }>;
-// 	}> = [];
-// 	// loop over initial list and get distance to all items in compareList
-// 	// sort them and append to a list of other items
-// 	for (let i = 0; i < list.length; i++) {
-// 		let distances = compareList.map((index) => ({
-// 			toIndex: index,
-// 			distance: _rotatedDistance(index, list[i]),
-// 		}));
-// 		if (distances.length) {
-// 			distances.sort(
-// 				(a, b) => Math.abs(a.distance) - Math.abs(b.distance)
-// 			);
-// 			minItems.push({
-// 				fromIndex: list[i],
-// 				distances,
-// 			});
-// 		}
-// 	}
-
-// 	// iterate over minItems, sorting by first item distance
-// 	// (to allow best candidates to get first dibs at being in result),
-// 	// Then loop over these sorted distance lists,
-// 	// if it can be assigned to result, then great, otherwise we remove and continue
-// 	// this will mutate minItems until it is array of empty items with empty distance arrays
-// 	const resultItems: { [key in number]: any } = {};
-// 	for (let i = 0; i < compareList.length; i++) {
-// 		// remove items that are found
-// 		minItems = minItems.filter(
-// 			(minItem) => resultItems[minItem.fromIndex] === undefined
-// 		);
-
-// 		// sort items by distance from next matching candidate
-// 		minItems.sort((a, b) => {
-// 			if (!a.distances.length && !b.distances.length) return 0;
-// 			if (!a.distances.length && b.distances.length) return 1;
-// 			if (a.distances.length && !b.distances.length) return -1;
-
-// 			return (
-// 				Math.abs(a.distances[0].distance) -
-// 				Math.abs(b.distances[0].distance)
-// 			);
-// 		});
-
-// 		for (let item of minItems) {
-// 			let distanceItem = item.distances.shift();
-// 			// check that toIndex is available and that building a path there isValid,
-// 			// IE doesnt cross another existing path
-// 			if (
-// 				available[distanceItem.toIndex] &&
-// 				Object.keys(resultItems).every((key) => {
-// 					const fromIndex = +key;
-// 					const toIndex = resultItems[fromIndex].toIndex;
-// 					return _isValid(
-// 						fromIndex,
-// 						toIndex,
-// 						item.fromIndex,
-// 						distanceItem.toIndex
-// 					);
-// 				})
-// 			) {
-// 				resultItems[item.fromIndex] = distanceItem;
-// 				available[distanceItem.toIndex] = false;
-// 			}
-// 		}
-// 	}
-
-// // build both directions
-// const rightResult: DiffType = {};
-// const leftResult: DiffType = {};
-// Object.keys(resultItems).forEach((fromIndex) => {
-// 	const { distance, toIndex } = resultItems[+fromIndex];
-// 	rightResult[+fromIndex] = distance;
-// 	leftResult[toIndex] = -distance;
-// });
-
-// // 9999 = fill
-// // -9999 = empty
-// for (let index of list) {
-// 	if (rightResult[index] === undefined) {
-// 		rightResult[index] = -9999;
-// 		leftResult[index] = 9999;
-// 	}
-// }
-
-// for (let index of compareList) {
-// 	if (available[+index]) {
-// 		rightResult[+index] = 9999;
-// 		leftResult[+index] = -9999;
-// 	}
-// }
-
-// 	return [leftResult, rightResult];
-// }
-
-// gets signed distance around the fretboard
-function _rotatedDistance(a: number, b: number, m: number = 12) {
-	let min = a - b;
-	if (Math.abs(a - b + m) < Math.abs(min)) min = a - b + m;
-	if (Math.abs(a - b - m) < Math.abs(min)) min = a - b - m;
-	return min;
-}
-
-function _isValid(fromA: number, toA: number, fromB: number, toB: number) {
-	// make sure paths dont cross. This is tricky because notes exist on a ring
-	// so we need to check that the shortest path between two points doesn't intersect another shortest path
-	// we do this by constucting a time graph of walking from point A to pointB, and seeing if they intersect
-
-	// ex:
-	// from 0 => 10 vs from 1 => 9
-	//
-	// t=0  1  2  3  4
-	//   0  11 10 10 10   (fill 10s to end of row since in time this path has stopped moving)
-	// - 1  0  11 10 9
-	//   -------------
-	//  -1  11 -1 0  1
-	//  -1  -1 -1 0  1   <== not all values have same sign, paths must cross
-
-	// if (fromA === 2 && fromB === 0 && toA === 2 && toB === 11) {
-	// 	console.log("in here");
-	// 	debugger;
-	// }
-
-	// adjustments for 0 => 11 issue
-	if (fromA < toA && toA - fromA > 6) fromA += 12; // puts above branch 1
-	if (fromB < toB && toB - fromB > 6) fromB += 12; // puts above branch 1
-	if (fromA > toA && fromA - toA > 6) toA += 12; // puts above branch 1
-	if (fromB > toB && fromB - toB > 6) toB += 12; // puts above branch 1
-
-	return !(fromA < fromB && toA > toB) && !(fromA > fromB && toA < toB); // check line intersects
-
-	// // from - steps == to
-	// const combinedDeltas: number[] = [];
-	// let listA: number[] = [];
-	// let listB: number[] = [];
-	// const stepsA = this._rotatedDistance(fromA, toA);
-	// const stepsB = this._rotatedDistance(fromB, toB);
-
-	// // build paths by walking in shortest direction
-	// // console.log(fromA, toA, stepsA);
-	// for (let i = 0; i <= Math.abs(stepsA); i++) {
-	// 	const increment = stepsA < 0 ? 1 : -1;
-	// 	listA.push(fromA + i * increment);
-	// }
-	// for (let i = 0; i <= Math.abs(stepsB); i++) {
-	// 	const increment = stepsB < 0 ? 1 : -1;
-	// 	listB.push(fromB + i * increment);
-	// }
-
-	// // make listA the longer list
-	// if (listB.length > listA.length) {
-	// 	let temp = listB;
-	// 	listB = listA;
-	// 	listA = temp;
-	// }
-
-	// // build delta list, which should all have the same sign (or be 0)
-	// for (let i = 0; i < listA.length; i++) {
-	// 	let val = listB[i];
-	// 	if (val === undefined) val = listB[listB.length - 1];
-	// 	let delta = listA[i] - val;
-	// 	// let min = delta;
-	// 	// if (Math.abs(delta + 12) < Math.abs(min)) min = delta + 12;
-	// 	// if (Math.abs(delta - 12) < Math.abs(min)) min = delta - 12;
-	// 	// combinedDeltas.push(this._rotatedDistance(listA[i], val));
-	// 	combinedDeltas.push(delta);
-	// }
-
-	// // check signs
-	// if (
-	// 	!combinedDeltas.every((d) => d <= 0) &&
-	// 	!combinedDeltas.every((d) => d >= 0)
-	// ) {
-	// 	return false;
-	// }
-
-	// return true;
-}
-
-// window.FretboardUtil = FretboardUtil;
-
-// window.fretboard1 = new FretboardUtil({
-// 	0: false,
-// 	1: false,
-// 	2: true,
-// 	3: false,
-// 	4: true,
-// 	5: false,
-// 	6: true,
-// 	7: false,
-// 	8: false,
-// 	9: true,
-// 	10: false,
-// 	11: true,
-// });
-
-// window.fretboard2 = new FretboardUtil({
-// 	0: true,
-// 	1: false,
-// 	2: true,
-// 	3: false,
-// 	4: true,
-// 	5: false,
-// 	6: false,
-// 	7: true,
-// 	8: false,
-// 	9: true,
-// 	10: false,
-// 	11: false,
-// });
-// window.compare = compare1;
